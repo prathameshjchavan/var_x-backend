dac467e6c3f8e0ac8a30a370fdfcd24e777b91ff
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2022-11-03 23:02:17 +0530

    added filter options

--- config/database.js
+++ config/database.js
@@ -2,15 +2,15 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST"),
-      port: env("DATABASE_PORT"),
-      database: env("DATABASE_NAME"),
-      user: env("DATABASE_USERNAME"),
-      password: env("DATABASE_PASSWORD"),
-      schema: env("DATABASE_SCHEMA"),
-      ssl: {
-        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
-      },
+      host: env("DATABASE_HOST_LOCAL"),
+      port: env("DATABASE_PORT_LOCAL"),
+      database: env("DATABASE_NAME_LOCAL"),
+      user: env("DATABASE_USERNAME_LOCAL"),
+      password: env("DATABASE_PASSWORD_LOCAL"),
+      schema: env("DATABASE_SCHEMA_LOCAL"),
+      // ssl: {
+      //   rejectUnauthorized: env.bool("DATABASE_SSL_SELF", true),
+      // },
     },
     debug: false,
   },
