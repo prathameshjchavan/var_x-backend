57ab864a58dadda7b18d5d440f68d4007e4a9ff8
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2022-12-21 16:28:20 +0530

    facebook provider configuration

--- config/server.js
+++ config/server.js
@@ -1,7 +1,8 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: env("", "https://1794-183-87-49-54.ngrok.io"),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
 });
