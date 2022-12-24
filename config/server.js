9bd7d0a6fd01890347b6c38291168aee8bd890a7
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2022-12-24 09:06:44 +0530

    strapi lifecycle hooks

--- config/server.js
+++ config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("", "https://1794-183-87-49-54.ngrok.io"),
   app: {
     keys: env.array("APP_KEYS"),
   },
