9902b6473d4de60b5bde8965409723290599f345
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-01-16 12:00:06 +0530

    create order controller

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -1,9 +1,38 @@
-'use strict';
+"use strict";
 
 /**
  * order controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::order.order');
+const GUEST_ID = 27;
+
+module.exports = createCoreController("api::order.order", ({ strapi }) => ({
+  async place(ctx) {
+    const {
+      shippingAddress,
+      billingAddres,
+      shippingInfo,
+      billingInfo,
+      shippingOption,
+      subtotal,
+      tax,
+      total,
+      items,
+    } = ctx.request.body;
+    let orderCustomer;
+    let serverTotal = 0;
+    let unavailable = [];
+
+    if (ctx.state.user) {
+      orderCustomer = ctx.state.user.id;
+    } else {
+      orderCustomer = GUEST_ID;
+    }
+
+    await Promise.all();
+
+    // only executed after everything in Promise.all has completed SUCCESSFULLY
+  },
+}));
