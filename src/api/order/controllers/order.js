d02e1aaf7a7548db062b3d531736755f9724d8a4
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-01-16 12:48:28 +0530

    server-side cart validation

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -24,6 +24,7 @@ module.exports = createCoreController("api::order.order", ({ strapi }) => ({
     let orderCustomer;
     let serverTotal = 0;
     let unavailable = [];
+    let invalid = false;
 
     if (ctx.state.user) {
       orderCustomer = ctx.state.user.id;
@@ -31,8 +32,53 @@ module.exports = createCoreController("api::order.order", ({ strapi }) => ({
       orderCustomer = GUEST_ID;
     }
 
-    await Promise.all();
+    await Promise.all(
+      items.map(async (clientItem) => {
+        const serverItem = await strapi
+          .service("api::order.order")
+          .findOne({ id: clientItem.variant.id });
+
+        if (serverItem.quantity < clientItem.qty) {
+          unavailable.push({ id: serverItem.id, qty: serverItem.quantity });
+        } else {
+          serverTotal += serverItem.price * clientItem.qty;
+
+          await strapi
+            .service("api::variant.variant")
+            .update(
+              { id: clientItem.variant.id },
+              { quantity: serverItem.quantity - clientItem.qty }
+            );
+        }
+      })
+    );
 
     // only executed after everything in Promise.all has completed SUCCESSFULLY
+    const shippingOptions = [
+      {
+        label: "FREE SHIPPING",
+        price: 0,
+      },
+      {
+        label: "2-DAY SHIPPING",
+        price: 9.99,
+      },
+      {
+        label: "OVERNIGHT SHIPPING",
+        price: 29.99,
+      },
+    ];
+    const shippingValid = shippingOptions.find(
+      (option) =>
+        option.label === shippingOption.label &&
+        option.price === shippingOption.price
+    );
+
+    if (
+      shippingValid === undefined ||
+      (serverTotal * 1.075 + shippingValid.price).toFixed(2) !== total
+    ) {
+      // invalid cart
+    }
   },
 }));
