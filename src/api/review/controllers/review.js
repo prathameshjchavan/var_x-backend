528f3e0d90728cb8e77ebe1fbfc466f3621aa2eb
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-01-30 12:45:21 +0530

    product average rating

--- src/api/review/controllers/review.js
+++ src/api/review/controllers/review.js
@@ -5,6 +5,15 @@
  */
 
 const { createCoreService } = require("@strapi/strapi").factories;
+const utils = require("@strapi/utils");
+const { sanitize } = utils;
+
+const sanitizeOutput = (review, ctx) => {
+  const schema = strapi.getModel("api::review.review");
+  const { auth } = ctx.state;
+
+  return sanitize.contentAPI.output(review, schema, { auth });
+};
 
 module.exports = createCoreService("api::review.review", ({ strapi }) => ({
   async create(ctx) {
@@ -12,47 +21,63 @@ module.exports = createCoreService("api::review.review", ({ strapi }) => ({
     let entity;
 
     body.user = ctx.state.user.id;
-    entity = await strapi.entityService.create("api::review.review", body);
+    entity = await strapi.entityService.create("api::review.review", {
+      data: body,
+      populate: {
+        product: true,
+      },
+    });
 
-    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
-    return this.transformResponse(sanitizedEntity);
+    await strapi.service("api::review.review").average(entity.product.id);
+
+    const sanitizedData = await sanitizeOutput(entity, ctx);
+
+    ctx.send(sanitizedData);
   },
   async update(ctx) {
     const { id } = ctx.params;
     let entity;
 
-    const review = await strapi.entityService.find("api::review.review", {
+    const review = await strapi.entityService.findOne(
+      "api::review.review",
       id,
-      user: ctx.state.user.id,
-    });
+      { populate: { user: true } }
+    );
 
-    if (!review) {
+    if (!review || review.user.id !== ctx.state.user.id) {
       return ctx.unauthorized(`You can't update this entry`);
     }
 
-    entity = await strapi.entityService.update(
-      "api::review.review",
-      id,
-      ctx.request.body
-    );
+    entity = await strapi.entityService.update("api::review.review", id, {
+      data: ctx.request.body,
+      populate: { product: true },
+    });
 
-    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
-    return this.transformResponse(sanitizedEntity);
+    await strapi.service("api::review.review").average(entity.product.id);
+
+    const sanitizedData = await sanitizeOutput(entity, ctx);
+
+    ctx.send(sanitizedData);
   },
   async delete(ctx) {
     const { id } = ctx.params;
 
-    const review = await strapi.entityService.find("api::review.review", {
+    const review = await strapi.entityService.findOne(
+      "api::review.review",
       id,
-      user: ctx.state.user.id,
-    });
+      { populate: { user: true } }
+    );
 
-    if (!review) {
+    if (!review || review.user.id !== ctx.state.user.id) {
       return ctx.unauthorized(`You can't delete this entry`);
     }
 
-    const response = await super.delete(ctx);
+    const entity = await strapi.entityService.delete("api::review.review", id, {
+      populate: { product: true },
+    });
 
-    return response;
+    await strapi.service("api::review.review").average(entity.product.id);
+
+    ctx.send("Deleted");
   },
 }));
--- src/api/review/services/review.js
+++ src/api/review/services/review.js
@@ -6,4 +6,23 @@
 
 const { createCoreService } = require("@strapi/strapi").factories;
 
-module.exports = createCoreService("api::review.review");
+module.exports = createCoreService("api::review.review", ({ strapi }) => ({
+  async average(id) {
+    const product = await strapi.entityService.findOne(
+      "api::product.product",
+      id,
+      { populate: { reviews: true } }
+    );
+    const total = product.reviews.reduce(
+      (total, review) => total + review.rating,
+      0
+    );
+    const average = !!product.reviews.length
+      ? total / product.reviews.length
+      : 0;
+
+    await strapi.entityService.update("api::product.product", id, {
+      data: { rating: Math.round(average * 2) / 2 },
+    });
+  },
+}));
